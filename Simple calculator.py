126bc7d53fd4223695d73614835311220a44d26a
Muskan Rastogi <muskanrastogi569@gmail.com>
2025-07-10 22:06:10 +0530

    Add files via upload

---
+++ Simple calculator.py
@@ -0,0 +1,21 @@
+# Simple calculator in Python
+
+# Input from user
+num1 = int(input("num1: "))
+num2 = int(input("num2: "))
+operation = input("operation (+, -, *, /): ")
+
+# Perform operation
+if operation == '+':
+    print(f"{num1} + {num2} = {num1 + num2}")
+elif operation == '-':
+    print(f"{num1} - {num2} = {num1 - num2}")
+elif operation == '*':
+    print(f"{num1} * {num2} = {num1 * num2}")
+elif operation == '/':
+    if num2 != 0:
+        print(f"{num1} / {num2} = {num1 / num2}")
+    else:
+        print("Division by zero is not allowed")
+else:
+    print("Invalid Operation")
